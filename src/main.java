29b8bd425e56440812be23437fa426f32cd10e78
Matthew Chow <matthewnchow@berkeley.edu>
2018-10-25 12:09:50 -0700

    Playing with delimiters -blank state issue rn

--- src/main.java
+++ src/main.java
@@ -16,7 +16,6 @@ public class main {
     private static ArrayList<String> _categories;
     private static HashMap<String, HashMap<String, Integer>> _counters;
     private static String _certKEY;
-    private static final String CERT = "CERTIFIED";
     private static int _certs;
 
     /** Get the input files, run the reader, write to output.
@@ -65,38 +64,53 @@ public class main {
      *  Then scans through each line, adding to the states and jobs HashMaps.*/
 	private static void process(Scanner s) {
         HashMap<Integer, String> cat_idx = new HashMap<>();
+        Scanner temp;
+        String line;
+        int catcount = 1;
         if (s.hasNextLine()) {
-            Scanner temp = new Scanner(s.nextLine());
+            temp = new Scanner(s.nextLine());
             temp.useDelimiter(";");
             for (int i = 0; temp.hasNext(); i++) {
 				String cat_i = temp.next();
-				if (_categories.contains(cat_i)) {cat_idx.put(i, cat_i);}
+                System.out.print(cat_i + Integer.toString(catcount) + ";");
+                catcount++;
+				if (_categories.contains(cat_i)) {cat_idx.put(i+1, cat_i);}
             }
+            System.out.println(cat_idx);
+            int badlns = 0;
             while (s.hasNextLine()) {
-                String line = s.nextLine();
+                line = s.nextLine();
                 if (line.contains(_certKEY)) {
                     _certs++;
                     temp = new Scanner(line);
                     temp.useDelimiter(";");
                     for (int i = 0; temp.hasNext(); i++) {
+                        temp.useDelimiter(";");
+                        if (temp.hasNext("(\").*")) {
+                            temp.useDelimiter("(\".*;)");
+                        }
                         String data = temp.next();
                         if (cat_idx.containsKey(i)) {
+                            if (!data.matches("([A-Z][A-Z])")) {
+                                badlns+=1;
+                                System.out.println(line);
+                            }
+
                             HashMap<String, Integer> temp_pointer =
                                     _counters.get(cat_idx.get(i));
                             if (temp_pointer.containsKey(data)) {
                                 temp_pointer.replace(data, temp_pointer.get(data) + 1);
-                            } else {
-                                temp_pointer.put(data, 1);
-                            }
+                            } else {temp_pointer.put(data, 1);}
                         }
                     }
                 }
 			}
-
-            System.out.println(_categories);
-            System.out.println(_counters);
-            System.out.println(_certs);
+            System.out.println(badlns);
         }
+        System.out.println(_categories);
+        System.out.println(_counters.get(_categories.get(0)));
+//        System.out.println(_counters.get(_categories.get(1)));
+        System.out.println(_certs);
     }
 
     private static void write_out(){
