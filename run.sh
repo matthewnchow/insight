fa04d15afdbec1ef1667600104284b3a4714536b
Matthew Chow <matthewnchow@berkeley.edu>
2018-10-30 00:25:45 -0700

    Before going back to working commit

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# A bash script to compile and run single java class
-# Java class main takes arguments ("CERTKEY, CATEGORY1_KEYWORDS, CATEGORY2_KEYWORDS, ... CATEGORYN_KEYWORDS")
-# written by Matthew Chow
-#javac src/main.java
-#java main(CERTIFIED, WORK-STATE, JOB-TITLE)
-
-javac src/main.java
-java main CERTIFIED WORK-STATE JOB-TITLE
